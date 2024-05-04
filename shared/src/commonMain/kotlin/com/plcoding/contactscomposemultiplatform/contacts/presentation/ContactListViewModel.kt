cd96cef2ba8f43f7c44853e861e853320fbfba28
hamza <hamza.919p@gmail.com>
2024-05-04 18:34:37 +0200

    combined different states in viewmodel

--- shared/src/commonMain/kotlin/com/plcoding/contactscomposemultiplatform/contacts/presentation/ContactListViewModel.kt
+++ shared/src/commonMain/kotlin/com/plcoding/contactscomposemultiplatform/contacts/presentation/ContactListViewModel.kt
@@ -4,31 +4,48 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import com.plcoding.contactscomposemultiplatform.contacts.domain.Contact
+import com.plcoding.contactscomposemultiplatform.contacts.domain.ContactDataSource
 import dev.icerock.moko.mvvm.viewmodel.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 
-class ContactListViewModel : ViewModel() {
-    private val _state = MutableStateFlow(ContactListState(
-        contacts = contacts
-    ))
-    val state = _state.asStateFlow()
+class ContactListViewModel(
+    private val contactDataSource: ContactDataSource
+) : ViewModel() {
+    private val _state = MutableStateFlow(ContactListState())
+    val state = combine(
+        _state,
+        contactDataSource.getContacts(),
+        contactDataSource.getRecentContacts()
+    ) { state, contacts, recentContacts ->
+
+        state.copy(
+            contacts = contacts,
+            recentlyAddedContacts = recentContacts
+        )
+
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000L), ContactListState())
 
     var newContact: Contact? by mutableStateOf(null)
         private set
 
     fun onEvent(event: ContactListEvent) {
-
+        when (event) {
+            ContactListEvent.DeleteContact -> TODO()
+            ContactListEvent.DismissContact -> TODO()
+            is ContactListEvent.EditContact -> TODO()
+            ContactListEvent.OnAddNewContactClick -> TODO()
+            ContactListEvent.OnAddPhotoClicked -> TODO()
+            is ContactListEvent.OnEmailChanged -> TODO()
+            is ContactListEvent.OnFirstNameChanged -> TODO()
+            is ContactListEvent.OnLastNameChanged -> TODO()
+            is ContactListEvent.OnPhoneNumberChanged -> TODO()
+            is ContactListEvent.OnPhotoPicked -> TODO()
+            ContactListEvent.SaveContact -> TODO()
+            is ContactListEvent.SelectContact -> TODO()
+        }
     }
-}
-
-val contacts = (1..50).map{
-    Contact(
-        id = it.toLong(),
-        firstName = "First$it",
-        lastName = "Last$it",
-        email = "testing$it@gmail.com",
-        phoneNumber = "12345678",
-        photoBytes = null
-    )
 }
